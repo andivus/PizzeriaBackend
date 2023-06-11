6eb765fbad3c5bc13060827b091b32c3a1b767f9
Andiv <div2005.kk@gmail.com>
2023-06-11 08:40:21 +0300

    feat: added CORS and Default Headers

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,8 @@ dependencies {
     implementation("io.ktor:ktor-server-resources:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-cors-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-default-headers-jvm:$ktor_version")
 
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
--- src/main/kotlin/ua/vn/div/plugins/HTTP.kt
+++ src/main/kotlin/ua/vn/div/plugins/HTTP.kt
@@ -1,11 +1,45 @@
 package ua.vn.div.plugins
 
+import io.ktor.http.*
 import io.ktor.server.plugins.openapi.*
 import io.ktor.server.routing.*
 import io.ktor.server.plugins.swagger.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.defaultheaders.*
 
 fun Application.configureHTTP() {
+    install(CORS) {
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Patch)
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
+        allowHeader(HttpHeaders.Authorization)
+        allowHeader("MyCustomHeader")
+        allowCredentials = true
+        allowHeader(HttpHeaders.ContentType)
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+
+    routing {
+        options("{...}") {
+            // Handle the preflight request
+            // Set the appropriate CORS headers
+            // For example:
+            call.response.headers.append(HttpHeaders.AccessControlAllowOrigin, "http://localhost:4200")
+            call.response.headers.append(HttpHeaders.AccessControlAllowHeaders, "*")
+            call.response.headers.append(HttpHeaders.AccessControlAllowMethods, "*")
+            call.response.headers.append(HttpHeaders.AccessControlAllowCredentials, "true")
+            call.response.status(HttpStatusCode.OK)
+        }
+    }
+
+    install(DefaultHeaders) {
+        header("X-Engine", "Ktor") // will send this header with each response
+    }
+
     routing {
         openAPI(path = "openapi")
     }
